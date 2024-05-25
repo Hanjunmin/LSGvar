ed5e7810eabf0df39090cb26f7129d31e48b6ef5
Junmin <98016418+Hanjunmin@users.noreply.github.com>
2024-05-25 13:35:40 +0800

    Update splitfile.sh

--- scripts/splitfile.sh
+++ scripts/splitfile.sh
@@ -1,13 +1,12 @@
 folder=$1
 mkdir ${folder}
+#sed -i '1,5s/^#[^#]/#&/' $2
 # 38:36
 # 19:40
 awk -F'\t' '{
-    if(NR < 40){  
+    if($0 ~ /^#/){  
         print $0
-    } else if (NR==40){
-        print "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tHG002";
-        } else{
+    } else{
             OFS="\t";
             $11=(length($4)-length($5) >0 ? length($4)-length($5) : length($5)-length($4))
             print $1, $2, $3, $4, $5, $6, $7, $8, $9,$10, $11
@@ -15,14 +14,12 @@ awk -F'\t' '{
 }' $2   > "${folder}/count.vcf"  #"${nowdic}/macigarsdr.vcf"
 
 less -S "${folder}/count.vcf" |awk -F'\t' '{
-    if(NR<40){
+   if($0 ~ /^#/){ 
         print $0
-    }else if (NR==40){
-        next
-        }else{
+    }else{
             OFS="\t";
             if($11>=50){
-                 print $1, $2, ".", $4, $5, ".", ".", ".", $9,$10
+                 print $1, $2, $3, $4, $5, $6, $7, $8, $9,$10
                 }else{
                     next
                 }
@@ -30,14 +27,12 @@ less -S "${folder}/count.vcf" |awk -F'\t' '{
 }' >"${folder}/SV.vcf"
 
 less -S "${folder}/count.vcf"  |awk -F'\t' '{
-    if(NR<40){
+    if($0 ~ /^#/){ 
         print $0
-    }else if (NR==40){
-        next
-        }else{
+    }else{
             OFS="\t";
             if($11<50 && $11>0){
-                 print $1, $2, ".", $4, $5, ".", ".", ".", $9,$10
+                 print $1, $2, $3, $4, $5, $6, $7, $8, $9,$10
                 }else{
                     next
                 }
@@ -45,14 +40,12 @@ less -S "${folder}/count.vcf"  |awk -F'\t' '{
 }' >"${folder}/indel.vcf"
 
 less -S "${folder}/count.vcf"  |awk -F'\t' '{
-    if(NR<40){
+     if($0 ~ /^#/){ 
         print $0
-    }else if (NR==40){
-        next
-        }else{
+    }else{
             OFS="\t";
             if($11==0){
-                 print $1, $2, ".", $4, $5, ".", ".", ".", $9,$10
+                 print $1, $2, $3, $4, $5, $6, $7, $8, $9,$10
                 }else{
                     next
                 }
