729242fe211c5baa2c05144c3c4d7ae7a2906368
z22jy <105220298+z22jy@users.noreply.github.com>
2024-12-14 19:12:53 +0800

    Update CIGAR.py

--- scripts/CIGAR.py
+++ scripts/CIGAR.py
@@ -1,5 +1,5 @@
-## module load bedtools
-
+##module load bedtools
+##load packages
 import subprocess
 from Bio import SeqIO
 import re
@@ -15,142 +15,138 @@ parser.add_argument('--o', help='output dir')
 args = parser.parse_args()
 print(args.p)
 
-
 QUERY =args.q
 REF =args.r
 paf=args.paf
-#paf="/home/jmhan/CIGAR/T2T/cigartest.paf"
-subprocess.run(f"awk '{{print $1,$3,$4,\"\",\"\",$5}}' {paf}|tr ' ' '\t'  > query.bed", shell=True)
+
+##get reference and query genome sequences
+subprocess.run(f"awk '{{print $1,$3,$4,\"\",\"\",$5}}' {paf} | tr ' ' '\t'  > query.bed", shell=True)
 subprocess.run(f"bedtools getfasta -fi {QUERY} -bed query.bed -s -fo que.fa", shell=True)
-
-subprocess.run(f"awk '{{print $6,$8,$9}}' {paf}|tr ' ' '\t'  > ref.bed", shell=True)  ##如果参考基因组对应的是正链不进行反转，如果是负链就进行反转
+subprocess.run(f"awk '{{print $6,$8,$9}}' {paf} | tr ' ' '\t'  > ref.bed", shell=True) ##notice the oriention of reference genome
 subprocess.run(f"bedtools getfasta -fi {REF} -bed ref.bed -fo ref.fa", shell=True)
-
-
 
 refsequences = {record.id: record.seq for record in SeqIO.parse('ref.fa', 'fasta')}
 querysequences = {record.id: record.seq for record in SeqIO.parse('que.fa', 'fasta')}
 
+##call SV from CIGAR
+def process_cigar(line):
+    columns = line.split('\t')
+    minus = columns[4]  ##Determine strand
+    ref_chr = columns[5]
+    ref_start = columns[7]
+    ref_end = columns[8]
+    query_chr = columns[0]
+    query_start = columns[2]
+    query_end = columns[3]
+    alllen=int(query_end)-int(query_start)
+    cigar_str = next((xx for xx in columns if xx.startswith('cg:Z:')), None)  ##get CIGAR string
+    cigar_str = cigar_str[5:]
+    selected_reffa = refsequences[ref_chr+":"+ref_start+"-"+ref_end]
+    selected_quefa = querysequences[query_chr+":"+query_start+"-"+query_end+"("+minus+")"]
+    id=ref_chr+":"+ref_start+"-"+ref_end+'_'+query_chr+":"+query_start+"-"+query_end
+    ref_pos = 0
+    query_pos = 0
+    results = []
 
-def process_line(line):
-	columns = line.split('\t')
-	minus = columns[4]  ## 判断正负链
-	ref_chr = columns[5]
-	ref_start = columns[7]
-	ref_end = columns[8]
-	query_chr = columns[0]
-	query_start = columns[2]
-	query_end = columns[3]
-	for xx in columns:
-		if xx.startswith('cg'):
-			CIGARstr = xx
-			break
-	index_of_first_digit = next((index for index, char in enumerate(CIGARstr) if char.isdigit()), None)
-	CIGARstr =CIGARstr[index_of_first_digit:]
-	selected_reffa = refsequences[ref_chr+":"+ref_start+"-"+ref_end]
-	selected_quefa = querysequences[query_chr+":"+query_start+"-"+query_end+"("+minus+")"]
-	id=ref_chr+":"+ref_start+"-"+ref_end+'_'+query_chr+":"+query_start+"-"+query_end
-	matches = re.finditer(r'(\d+[DXI])', CIGARstr) # SNP
-	alllen=int(query_end)-int(query_start)
-	result_lines = []
-	for match in matches:
-		ref_posf=sum(list(map(int, re.findall(r'\d+',''.join(re.findall(r'(\d+[=XD])', CIGARstr[:match.start()]))))))
-		query_posf=sum(list(map(int, re.findall(r'\d+',''.join(re.findall(r'(\d+[=XI])', CIGARstr[:match.start()]))))))
-		if(match.group(1)=="1D"):
-			ref_strseq=selected_reffa[ref_posf-1:ref_posf+1] ## ref_posf-1的位置是一样的
-			que_strseq=selected_quefa[query_posf-1:query_posf] ##原来为空
-			if(minus=="-"):
-				result_lines.append(f"{id}\t{ref_posf+1}\t{alllen-query_posf}\t1\tSNP_DEL\t{ref_strseq}\t{que_strseq}\t{minus}\n") ## ref_posf ref_posf+1 query_posf query_posf 
-			else:
-				result_lines.append(f"{id}\t{ref_posf+1}\t{query_posf+1}\t1\tSNP_DEL\t{ref_strseq}\t{que_strseq}\t{minus}\n") ## ref_posf ref_posf+1 query_posf query_posf 
-			
-		if(match.group(1)=="1I"):
-			ref_strseq=selected_reffa[ref_posf-1:ref_posf] #原先为空
-			que_strseq=selected_quefa[query_posf-1:query_posf+1]
-			
-			if(minus=="-"):
-				result_lines.append(f"{id}\t{ref_posf+1}\t{alllen-query_posf}\t1\tSNP_INS\t{ref_strseq}\t{que_strseq}\t{minus}\n")## ref_posf ref_posf query_posf query_posf+1 
-			else:
-				result_lines.append(f"{id}\t{ref_posf+1}\t{query_posf+1}\t1\tSNP_INS\t{ref_strseq}\t{que_strseq}\t{minus}\n")## ref_posf ref_posf query_posf query_posf+1 
-			
-		if(match.group(1)=="1X"):
-			ref_strseq=selected_reffa[ref_posf:ref_posf+1]  ### 左闭右开，所以ref_posf个相等（最后一个位置是ref_posf-1），那新的变异应该是ref_posf,但是实际的位置应该是ref_posf+1
-			que_strseq=selected_quefa[query_posf:query_posf+1]
-			if(minus=="-"):
-				result_lines.append(f"{id}\t{ref_posf+1}\t{alllen-query_posf}\t1\tSNP\t{ref_strseq}\t{que_strseq}\t{minus}\n") ## ref_posf ref_posf+1 query_posf query_posf+1 
-			else:
-				result_lines.append(f"{id}\t{ref_posf+1}\t{query_posf+1}\t1\tSNP\t{ref_strseq}\t{que_strseq}\t{minus}\n") ## ref_posf ref_posf+1 query_posf query_posf+1 
-			
-			
-	# indel
-	# ins
-	matches = re.finditer(r'((?!1[DI])\d+[I])', CIGARstr)
-	for match in matches:
-		len=int(re.findall(r'\d+',match.group(1))[0])
-		ref_posf=sum(list(map(int, re.findall(r'\d+',''.join(re.findall(r'(\d+[=XD])', CIGARstr[:match.start()]))))))
-		query_posf=sum(list(map(int, re.findall(r'\d+',''.join(re.findall(r'(\d+[=XI])', CIGARstr[:match.start()]))))))
-		ref_strseq=selected_reffa[ref_posf-1:ref_posf] ##原来为空
-		que_strseq=selected_quefa[query_posf-1:query_posf+len]
-		if(minus=="-"):
-			result_lines.append(f"{id}\t{ref_posf+1}\t{alllen-query_posf-len+1}\t{len}\tINDEL_INS\t{ref_strseq}\t{que_strseq}\t{minus}\n")
-		else:
-			result_lines.append(f"{id}\t{ref_posf+1}\t{query_posf+1}\t{len}\tINDEL_INS\t{ref_strseq}\t{que_strseq}\t{minus}\n")
-			
-		
-	matches = re.finditer(r'((?!1[DI])\d+[D])', CIGARstr)  ## del
-	for match in matches:
-		len=int(re.findall(r'\d+',match.group(1))[0])
-		ref_posf=sum(list(map(int, re.findall(r'\d+',''.join(re.findall(r'(\d+[=XD])', CIGARstr[:match.start()]))))))
-		query_posf=sum(list(map(int, re.findall(r'\d+',''.join(re.findall(r'(\d+[=XI])', CIGARstr[:match.start()]))))))
-		ref_strseq=selected_reffa[ref_posf-1:ref_posf+len]
-		que_strseq=selected_quefa[query_posf-1:query_posf] #原来为空
-		if(minus=="-"):
-			result_lines.append(f"{id}\t{ref_posf+1}\t{alllen-query_posf}\t{len}\tINDEL_DEL\t{ref_strseq}\t{que_strseq}\t{minus}\n")
-		else:
-			result_lines.append(f"{id}\t{ref_posf+1}\t{query_posf+1}\t{len}\tINDEL_DEL\t{ref_strseq}\t{que_strseq}\t{minus}\n")		
-	
+    ##find all cigar pairs(numbers+types) and record the variations along the string
+    i = 0
+    while i < len(cigar_str):
+        ##search numbers
+        num_match = re.match(r'(\d+)', cigar_str[i:])
+        if num_match:
+            length = int(num_match.group(1))
+            i += len(num_match.group(1)) 
 
-	matches = re.finditer(r'((?!1[DIX])\d+[X])', CIGARstr)
-	for match in matches:
-		len=int(re.findall(r'\d+',match.group(1))[0])
-		j=0
-		for i in range(len):
-			ref_posf=sum(list(map(int, re.findall(r'\d+',''.join(re.findall(r'(\d+[=XD])', CIGARstr[:match.start()]))))))+j
-			query_posf=sum(list(map(int, re.findall(r'\d+',''.join(re.findall(r'(\d+[=XI])', CIGARstr[:match.start()]))))))+j
-			ref_strseq=selected_reffa[ref_posf:ref_posf+1] 
-			que_strseq=selected_quefa[query_posf:query_posf+1]
-			if(minus=="-"):
-				result_lines.append(f"{id}\t{ref_posf+1}\t{alllen-query_posf}\t1\tSNP\t{ref_strseq}\t{que_strseq}\t{minus}\n")
-			else:
-				result_lines.append(f"{id}\t{ref_posf+1}\t{query_posf+1}\t1\tSNP\t{ref_strseq}\t{que_strseq}\t{minus}\n")		
-			j=j+1
+            #search cigar digit（=, X, D, I）
+            if i < len(cigar_str) and cigar_str[i] in '=XDI':
+                variant_type = cigar_str[i]
+                i += 1  ##move to the next pair
+                
+                ##1.match
+                if variant_type == '=':
+                    ref_pos += length
+                    query_pos += length
+                    
+                ##2.mismatch(SNP)
+                elif variant_type == 'X':
+                    if length == 1:
+                        ref_strseq=selected_reffa[ref_pos:ref_pos+1] 
+                        que_strseq=selected_quefa[query_pos:query_pos+1]
+                        if(minus=="-"):
+                            results.append(f"{id}\t{ref_pos+1}\t{alllen-query_pos}\t1\tSNP\t{ref_strseq}\t{que_strseq}\t{minus}\n") 
+                        else:
+                            results.append(f"{id}\t{ref_pos+1}\t{query_pos+1}\t1\tSNP\t{ref_strseq}\t{que_strseq}\t{minus}\n")
+                        ref_pos += 1
+                        query_pos += 1
+                    else:
+                        for _ in range(length):  ##split into multi SNPs
+                            ref_strseq=selected_reffa[ref_pos:ref_pos+1] 
+                            que_strseq=selected_quefa[query_pos:query_pos+1]
+                            if(minus=="-"):
+                                results.append(f"{id}\t{ref_pos+1}\t{alllen-query_pos}\t1\tSNP\t{ref_strseq}\t{que_strseq}\t{minus}\n")
+                            else:
+                                results.append(f"{id}\t{ref_pos+1}\t{query_pos+1}\t1\tSNP\t{ref_strseq}\t{que_strseq}\t{minus}\n")		
+                            ref_pos += 1
+                            query_pos += 1
+                            
+                ##3.deletion
+                elif variant_type == 'D':
+                    if length == 1:
+                        ref_strseq=selected_reffa[ref_pos-1:ref_pos+1] 
+                        que_strseq=selected_quefa[query_pos-1:query_pos]
+                        if(minus=="-"):
+                            results.append(f"{id}\t{ref_pos+1}\t{alllen-query_pos}\t1\tSNP_DEL\t{ref_strseq}\t{que_strseq}\t{minus}\n") 
+                        else:
+                            results.append(f"{id}\t{ref_pos+1}\t{query_pos+1}\t1\tSNP_DEL\t{ref_strseq}\t{que_strseq}\t{minus}\n")
+                    else:
+                        ref_strseq=selected_reffa[ref_pos-1:ref_pos+length] 
+                        que_strseq=selected_quefa[query_pos-1:query_pos]
+                        if(minus=="-"):
+                            results.append(f"{id}\t{ref_pos+1}\t{alllen-query_pos}\t{length}\tINDEL_DEL\t{ref_strseq}\t{que_strseq}\t{minus}\n")
+                        else:
+                            results.append(f"{id}\t{ref_pos+1}\t{query_pos+1}\t{length}\tINDEL_DEL\t{ref_strseq}\t{que_strseq}\t{minus}\n")
+                    ref_pos += length
+                
+                ##4.insertion
+                elif variant_type == 'I':
+                    if length == 1:
+                        ref_strseq=selected_reffa[ref_pos-1:ref_pos] 
+                        que_strseq=selected_quefa[query_pos-1:query_pos+1]
+                        if(minus=="-"):
+                            results.append(f"{id}\t{ref_pos+1}\t{alllen-query_pos}\t1\tSNP_INS\t{ref_strseq}\t{que_strseq}\t{minus}\n")## ref_posf ref_posf query_posf query_posf+1 
+                        else:
+                            results.append(f"{id}\t{ref_pos+1}\t{query_pos+1}\t1\tSNP_INS\t{ref_strseq}\t{que_strseq}\t{minus}\n")
+                    else:
+                        ref_strseq=selected_reffa[ref_pos-1:ref_pos] 
+                        que_strseq=selected_quefa[query_pos-1:query_pos+length]
+                        if(minus=="-"):
+                            results.append(f"{id}\t{ref_pos+1}\t{alllen-query_pos-length+1}\t{length}\tINDEL_INS\t{ref_strseq}\t{que_strseq}\t{minus}\n")
+                        else:
+                            results.append(f"{id}\t{ref_pos+1}\t{query_pos+1}\t{length}\tINDEL_INS\t{ref_strseq}\t{que_strseq}\t{minus}\n")
+                    query_pos += length
+            else:
+                ##other types:add length to the ref and query positions
+                ref_pos += length
+                query_pos += length
+        else:
+            break  
+            
+    with open("temp/"+ref_chr+":"+str(ref_start)+"-"+str(ref_end)+"_"+query_chr+":"+str(query_start)+"-"+str(query_end)+".cigar", 'w') as outfile:
+        for line in results:
+            outfile.write(line)
+    return results
 
-	with open("temp/"+ref_chr+":"+ref_start+"-"+ref_end+"_"+query_chr+":"+query_start+"-"+query_end+".cigar", 'w') as outfile:
-		for line in result_lines:
-			outfile.write(line)
-	return result_lines	
 
-	
 
 a=time.time()
 with open(paf, 'r') as infile, open(args.o, 'w') as outfile:
 	write=outfile.write(f"id\tref_start\tquery_start\tlen\ttype\tref_seq\tquery_seq\tQUERYSTRAND\n")
 	with Pool(processes=int(args.p)) as pool:
-		## 之前是30
-		results = pool.map(process_line, infile)
+		results = pool.map(process_cigar, infile)
 	b=time.time()
-	print((a-b)/60)
-
-
+print(f"Processing time: {(b-a)/60:.2f} minutes")
+ 
 subprocess.run(f"rm query.bed", shell=True)
 subprocess.run(f"rm que.fa", shell=True)
-
-subprocess.run(f"rm ref.bed", shell=True)  ##如果参考基因组对应的是正链不进行反转，如果是负链就进行反转
+subprocess.run(f"rm ref.bed", shell=True)  
 subprocess.run(f"rm ref.fa", shell=True)
-# with open(paf, 'r') as infile:
-# 	all_lines = infile.readlines()
-
-
-
-# for match in matches:
-# 	print(match)
