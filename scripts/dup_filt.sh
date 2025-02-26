acf9664374eee2ef9fe7d4adc05022a73adb9fa9
Feifei Zhou <105220298+z22jy@users.noreply.github.com>
2025-02-26 12:06:16 +0800

    Update dup_filt.sh

--- scripts/dup_filt.sh
+++ scripts/dup_filt.sh
@@ -1,71 +1,35 @@
-#set -e
+#!/bin/bash
+source config.json
 paf=$1
 file=$2
 out=$3
-# paf="/home/jmhan/SDR/HG002/afterchaos_38_002ma.filt.paf" #
-# file="/home/jmhan/SDR/HG002/macigar.txt"  #
-# out="38_002_macigiarout.txt" #
 
+sort -k8,8n "$paf" |awk '{OFS=FS="\t"}{print $6,$8,$9}' > refpaf.region
+awk '{count[$0]++} END {for (line in count) if (count[line] > 1) print line}' refpaf.region > duplicates.txt
+bedtools intersect -a refpaf.region -b refpaf.region | awk 'NR==FNR{a[$0];next} !($0 in a)' refpaf.region - | sort -k1,1V -k2,2n -k3,3n | uniq | bedtools merge -i - > intersect.txt
+cat duplicates.txt intersect.txt | sort -k1,1V -k2,2n -k3,3n > mergedup.txt
 
-# paf="/home/jmhan/SDR/HG002/GRCH37/afterchaos_37_002pa.filt.paf" #
-# file="//home/jmhan/SDR/HG002/GRCH37/pacigar.txt"  #
-# out="37_002_pacigiarout.txt" #
-less -S $paf |less -S |sort -k8,8n |less -S  |awk '{OFS=FS="\t"}{print $6,$8,$9}' |less -S >refpaf.region
-awk 'NR==FNR{a[$0];next} !($0 in a)' refpaf.region <(bedtools intersect -a refpaf.region -b refpaf.region)  |sort -k1,1V -k2,2n -k3,3n |uniq|bedtools merge -i - |less -S >mergedup.txt
-less -S $file | awk -F'[:-]' 'BEGIN {OFS="\t"}{print $0,$1,$2}' |less -S |awk '{OFS=FS="\t";print $0,$10+$2}' |nl >cigartestin.txt
-cat /dev/null > del.txt
+awk -F'[:-]' 'BEGIN {OFS="\t"}{print $0,$1,$2}' $file | awk '{OFS=FS="\t";print $0,$10+$2}' | nl > cigartestin.txt
 
+awk -v OFS='\t' 'NR>1 {print $10, $11, $12, $1, $2}' cigartestin.txt > cigartestin.bed
 
-less -S mergedup.txt |less -S |while read -r line;do
-echo $line
-chr=$(echo $line |cut -d' ' -f 1)
-start=$(echo $line |cut -d' ' -f 2)
-end=$(echo $line |cut -d' ' -f 3)
-less -S cigartestin.txt |awk -v chr="$chr" -v start="$start" -v end="$end" -F"\t" '$10==chr && $12>=start && $12<=end {print $0}' >test.txt
-nr=$(less -S test.txt |wc -l)
-if [ "$nr" -ne 0 ]; then
-sampleuniq=$(less -S test.txt |cut -f 2 |sort |uniq |wc -l)
-if [ "$sampleuniq" -ne 1 ];then
-mapfile -t sampleuniqlist < <(less -S test.txt | cut -f 2 | sort | uniq)
-for m in "${sampleuniqlist[@]}";do
-seqstart=$(less -S test.txt |awk -v m="$m" '$2==m{print $0}'|sort -k12,12n |less -S |head -1 |cut -f 12)
-seqend=$(less -S test.txt |awk -v m="$m" '$2==m{print $0}'|sort -k12,12n |less -S |tail -1 |cut -f 12)
-seqnum=$(less -S test.txt |awk -v m="$m" '$2==m{print $0}' |wc -l)
-if [ "$seqend" -ne "$seqstart" ];then
-	freq=$(echo "scale=10; $seqnum / ($seqend - $seqstart)" | bc)
-	echo $m,$freq
-fi
-done >sample_freq.txt
-sam_len=$(less -S sample_freq.txt |wc -l)
-if [ "$sam_len" -ne 0 ];then
-mapfile -t sample_del < <(less -S sample_freq.txt | sort -t"," -k2,2g | awk -F"," 'NR>=2{print $1}')
-for sample in "${sample_del[@]}";do
-less -S test.txt |awk -v sample="$sample" '$2==sample{print $1}'
-done >>del.txt
-fi
-fi
-fi
-done
+bedtools intersect -a mergedup.txt -b cigartestin.bed -wa -wb > overlaps.txt
+awk -F'\t' '{if ($6 >= $2 && $6 <= $3) {print}}' overlaps.txt > overlaps_filt.txt
 
-if [ -s del.txt ]; then
-    awk 'NR==FNR{seq[$1]; next} !($1 in seq)' del.txt cigartestin.txt |awk 'OFS=FS="\t"{print $2,$3,$4,$5,$6,$7,$8,$9}'  > $out
+if [[ -s overlaps_filt.txt ]]; then
+    python "${tool_path}/scripts/dup_filt.py" overlaps_filt.txt fp
+    cat *_del.txt > del.txt
+    
+    if [ -s del.txt ]; then
+        awk 'NR==FNR{d[$1]; next} !($1 in d)' del.txt cigartestin.txt | 
+        cut -f 2-9 > "$out"
+    else
+        cut -f 2-9 cigartestin.txt > "$out"
+    fi
 else
-    cat cigartestin.txt |awk 'OFS=FS="\t"{print $2,$3,$4,$5,$6,$7,$8,$9}'  > $out
+    cut -f 2-9 cigartestin.txt > "$out"
 fi
 
-
-rm cigartestin.txt && rm refpaf.region && rm del.txt && rm sample_freq.txt && rm mergedup.txt && rm test.txt
-
-## test
-# less -S mergedup.txt | less -S | while read -r line; do
-#     chr=$(echo $line | cut -d' ' -f 1)
-#     start=$(echo $line | cut -d' ' -f 2)
-#     end=$(echo $line | cut -d' ' -f 3)
-#     less -S macigartestout.txt | awk -v chr="$chr" -v start="$start" -v end="$end" -F"\t" '$10==chr && $12>=start && $12<=end {print $0}' > test.txt
-#     nr=$(less -S test.txt | wc -l)
-#     if [ "$nr" -ne 0 ]; then
-#         sampleuniq=$(less -S test.txt | cut -f 2 | sort | uniq | wc -l)
-#         echo $line
-#         echo $sampleuniq
-#     fi
-# done
+rm -f refpaf.region mergedup.txt cigartestin.txt cigartestin.bed overlaps.txt overlaps_filt.txt del.txt duplicates.txt intersect.txt
+rm *_del.txt
+rm *_frequencies.txt
